31c36ce3633b5233da2efd444e1d0c19fea0e710
Daniel T <jtena@ucol.mx>
2025-02-07 06:11:16 -0600

    Se agregan las primeras variables para trabajar el app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Primero se declara la lista donde se guardaran los participantes
+let participantes = [];
+// Se realiza la funcion que permite agregar los participantes a la lista
+function agregarParticipantes() {
+    //Se solicita que se ingrese el nombre del participante y se usa mediant un getEment
+        let nombre = document.getElementById("amigo").value; //se obtiene el nombre mediante el input
+        //Se verifica si el nombre no esta vacio
+    
+}
