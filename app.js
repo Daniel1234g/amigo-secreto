1ffa3369fc3e60f20155e1babf9a232a9cf1350c
Daniel T <jtena@ucol.mx>
2025-02-07 06:27:15 -0600

    Se corrije la funcion de agregar participante

--- app.js
+++ app.js
@@ -3,8 +3,20 @@
 let participantes = [];
 // Se realiza la funcion que permite agregar los participantes a la lista
 function agregarParticipantes() {
-    //Se solicita que se ingrese el nombre del participante y se usa mediant un getEment
-        let nombre = document.getElementById("amigo").value; //se obtiene el nombre mediante el input
-        //Se verifica si el nombre no esta vacio
-    
+    // Se obtiene el nombre del participante mediante el input
+    let nombre = document.getElementById("amigo").value;
+
+    // Se verifica si el nombre no está vacío
+    if (nombre === "") {
+        alert("Por favor ingrese el nombre del participante.");
+    } else if (participantes.includes(nombre)) { // Se verifica que el nombre no se repita
+        alert("Ese nombre ya se encuentra en la lista.");
+    } else {
+        participantes.push(nombre); // Se agrega el nombre a la lista
+        
+        mostrarLista(); // Se actualiza la lista en la pantalla
+    }
+
+    // Se limpia el input después de agregar el nombre
+    document.getElementById("amigo").value = "";
 }
